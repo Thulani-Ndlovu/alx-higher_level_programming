470dcebf737e35454aea2e73c3bc4f282d24f432
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-06-05 14:13:41 +0200

    Task 8. Create a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find("object-oriented programming "):str.find("object-oriented programming ")+28] + str[str.find("with"):str.find("with")+5] + str[str.find("Python"):str.find("Python")+6]
+str = str[39:67] + str[107:112] + str[0:6]
 print(str)
