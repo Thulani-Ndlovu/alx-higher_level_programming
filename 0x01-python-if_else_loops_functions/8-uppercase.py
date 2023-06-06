07e6ffb7384a8f227fb3be5557094b453fa77786
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-06-07 01:20:48 +0200

    Task 8. to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)):
-        if str[i] >= 'a' and str[i] <= 'z':
-            c = str[i]
-            c = ord(c)
-            c = c - 32
-            c = chr(c)
-            str = str[:i] + c + str[i+1:]
-        print('{}'.format(str[i]), end="")
-    print("\n")
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+            print("{}".format(i), end="")
+        print("")
