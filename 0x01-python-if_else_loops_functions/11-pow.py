bcb9b8a86987101bf61a3cc909360f462653e26b
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-06-07 02:12:53 +0200

    Task 11. a ^ b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return pow(a, b)
+    res = 1
+    if b > 0:
+        for i in range(b):
+            res *= a
+    else:
+        for i in range(abs(b)):
+            res *= (1/a)
+    return (res)
