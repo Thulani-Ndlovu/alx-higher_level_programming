78e1fa99ef8e37568779a4f92d8cfc2b5d921d61
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-06-08 23:21:10 +0200

    Task 6. Build my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
     from calculator_1 import add, sub, mul, div
+    import sys
     operator = "+-*/"
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    if (len(sys.argv)) != 4:
+
+    if (len(sys.argv)) < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
     else:
-        if (sys.argv[2] not in operator):
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if (sys.argv[2] not in "*+-/"):
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
         else:
