b70853e7b7751d36b34a59fdd9b31a0073bb89bc
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-06-14 20:34:56 +0200

    Task 11. Multiply by using map

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    copy_list = my_list.copy()
-    return list(map(lambda x: number * x, copy_list))
+        return list(map(lambda x: number * x, my_list))
