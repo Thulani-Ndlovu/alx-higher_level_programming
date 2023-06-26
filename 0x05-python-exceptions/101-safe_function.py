c91593ad21f02f5a1ac3dbf2570e85c99f4888b3
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-06-26 21:04:18 +0200

    Task 8. Safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     try:
         answer = fct(*args)
-    except TypeError:
+    except Exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         answer = None
     return answer
