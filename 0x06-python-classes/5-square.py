54f327f969edb4aabaab09032535ff88a0f09b40
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-06-27 20:48:54 +0200

    Task 5. Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+'''Class Square declaration'''
+
+
+class Square:
+    '''Square constructor'''
+    def __init__(self, size=0):
+        '''
+            Args:
+                size (int): integer value
+            '''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    '''Area of the square'''
+    def area(self):
+        return self.__size * self.__size
+
+    '''Size Getter'''
+    @property
+    def size(self):
+        return self.__size
+
+    '''Size Setter'''
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    '''Print Square characters'''
+    def my_print(self):
+        if self.__size > 0:
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print('#', end="")
+                print("")
+        elif self.__size == 0:
+            print("")
