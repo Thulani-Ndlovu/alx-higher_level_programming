5134acbd35c0298e0808c4d0a13798a487d3ebe4
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-06-27 17:26:15 +0200

    Task 1 Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+''' A Class name Square is defined.'''
 
 
 class Square:
+
+    ''' An init method is included to initialize size
+      as a private field name'''
 
     def __init__(self, size):
         self.__size = size
