5cd1c490ed3ae31f7173cea667666cec11f87f9b
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-07-03 15:20:45 +0200

    Task 2. Area and Perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -52,12 +52,12 @@ class Rectangle:
         else:
             self.__height = value
 
-    '''Area of a Triangle'''
+    '''Area of a rectangle'''
     def area(self):
         return self.__height * self.__width
 
-    '''Perimeter of a Triangle'''
+    '''Perimeter of a rectangle'''
     def perimeter(self):
-        if self.__height < 0 or self.__width < 0:
+        if self.__height == 0 or self.__width == 0:
             return 0
         return 2 * self.__width + 2 * self.__height
