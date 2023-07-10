70cff4d9b1a7b8849d091a8aa95beb99fc09e8b2
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-07-10 22:52:52 +0200

    Task 6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Class definition'''
+
+
+class BaseGeometry:
+    '''class implementation'''
+    def area(self):
+        """Raise exception"""
+        raise Exception("area() is not implemented")
