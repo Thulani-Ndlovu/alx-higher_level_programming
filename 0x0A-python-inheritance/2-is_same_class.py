ef4463de78a52c8faeb57742fa1ce784e232c95d
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-07-10 22:35:06 +0200

    Task 2. Same class or inherit from

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Function definition'''
+
+
+def is_same_class(obj, a_class):
+    '''returns true if object is an instance of a class'''
+    if type(obj) is a_class:
+        return True
+    return False
