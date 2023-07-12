4de53e885ae1bc806ba95339b96353efda88373c
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-07-12 15:00:38 +0200

    Task 9. Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Class definition'''
+
+
+class Student:
+    '''Constructor'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''retrieves a dictionary representation of a Student instance'''
+        return self.__dict__
