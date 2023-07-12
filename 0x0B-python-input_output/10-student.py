43cae2016ee3c759dfaefe544c7532789eded4c1
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-07-12 15:10:57 +0200

    Task 10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Class definition'''
+
+
+class Student:
+    '''Constructor'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''retrieves a dictionary representation of a Student instance'''
+        if (type(attrs) == list and
+                all(type(a) == str for a in attrs)):
+            return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
+        return self.__dict__
