e5365b6a50015a613837b14651773a48e012ff50
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-07-12 14:04:27 +0200

    Task 2. Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Function definition'''
+
+
+def append_write(filename="", text=""):
+    '''
+        appends a string at the end of a text file (UTF8) and
+        returns the number of characters added:
+    '''
+    with open(filename, "a", encoding="utf-8") as fl:
+        return fl.write(text)
