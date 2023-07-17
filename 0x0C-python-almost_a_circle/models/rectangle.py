a76ba5a406208fc9b7e33495ec9953a07da9abc0
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-07-17 17:25:35 +0200

    Task 4. Area first

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,3 +89,8 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    '''Area'''
+    def area(self):
+        '''returns the area'''
+        return self.__width * self.__height
