ec79feab8256ac7c2393d6e38af4e5fd89b69bcc
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-09-11 22:00:01 +0200

    Task 5. An integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const args = process.argv;
 if (!args[2] || !parseInt(args[2])) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', parseInt(args[2]));
+  console.log('My number: ' + parseInt(args[2]));
 }
