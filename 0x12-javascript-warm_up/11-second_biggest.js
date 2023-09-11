dc3c2f5c609e6cc1bb02b47a5db02d34ed4826f8
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-09-11 23:04:37 +0200

    Task 11. Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,12 +3,11 @@ const args = process.argv;
 const array = [];
 let sorted = [];
 let count = 0;
-if (!args[2] || args.length < 4) {
+if (args.length < 4) {
   console.log(0);
 } else {
   for (let i = 2; i < args.length; i++) {
-    array[count] = parseInt(args[i]);
-    count++;
+    array[count++] = parseInt(args[i]);
   }
   sorted = array.sort();
   console.log(sorted[sorted.length - 2]);
