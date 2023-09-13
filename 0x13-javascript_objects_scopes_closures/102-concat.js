f6af813b94fd01a7f3090344055cbdf41270e54a
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-09-13 14:18:10 +0200

    Task 13. Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,18 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 const args = process.argv;
-
-const fileA = args[1];
-const fileB = args[2];
-const fileC = args[3];
-
-function concatenateFiles (source1, source2, destination) {
-  if (args.length < 4) {
-    return -1;
-  }
-  const data1 = fs.readFileSync(source1, 'utf8');
-  const data2 = fs.readFileSync(source2, 'utf8');
-  const concatenatedData = data1 + '\n' + data2;
-  fs.writeFileSync(destination, concatenatedData);
-}
-concatenateFiles(fileA, fileB, fileC);
+const fileA = fs.readFileSync(args[2]).toString();
+const fileB = fs.readFileSync(args[3]).toString();
+fs.writeFileSync(args[4], fileA + fileB);
