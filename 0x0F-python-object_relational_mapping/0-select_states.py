670a7695e85b23c97ad30c4f5e54aa8e5f86d9bb
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-09-16 18:03:30 +0200

    Task 0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,9 +14,16 @@ def States(username, password, name):
         db=name
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM `states` ORDER BY `id` ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
 
     db.close()
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    States(username, password, database)
