1b9607eaf269fa4eef04e87e50d628b20b058f94
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-09-17 20:04:20 +0200

    Task 14. Cities in state

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Model city class which inherits Base class'''
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class city(Base):
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
