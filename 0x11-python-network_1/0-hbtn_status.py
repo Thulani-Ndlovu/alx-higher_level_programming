2da99186a1bed8e712fa73a463ebe0b27d274aba
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-10-02 21:43:19 +0200

    Task 0. Whats my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,8 @@
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as results:
+    with urllib.request.urlopen('https://alx-intranet.\
+                                hbtn.io/status')as results:
         info = results.read()
         print("Body response:")
         print("\t- type: {}".format(type(info)))
