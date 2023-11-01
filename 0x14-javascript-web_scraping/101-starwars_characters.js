7ea62ba1270e78033233c0b8a867bc59ae685d24
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-11-01 22:30:44 +0200

    Task 8. Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+function getData (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (e, res, body) => {
+      if (e) {
+        reject(e);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+}
+
+function errorHandler (e) {
+  console.log(e);
+}
+
+function movieCharacters (movieId) {
+  const movieAPI = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+  getData(movieAPI).then(JSON.parse, errorHandler).then((res) => {
+    const chars = res.characters;
+    const promises = {};
+
+    for (let i = 0; i < chars.length; ++i) {
+      promises.push(getData(chars[i]));
+    }
+
+    Promise.all(promises).then((result) => {
+      for (let j = 0; j < result.length; ++j) {
+        console.log(JSON.parse(result[j]).name);
+      }
+    }).catch((error) => {
+      console.log(error);
+    });
+  });
+}
+
+movieCharacters(process.argv[2]);
