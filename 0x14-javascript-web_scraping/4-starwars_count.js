83ab4b8953b8f445e873302bcd9e5abf493e0fca
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-11-01 21:15:56 +0200

    Task 4. Star Wars Wedge Antills

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,7 +5,7 @@ const starWarsAPI = process.argv[2];
 let count = 0;
 
 request(starWarsAPI, (e, res, body) => {
-  body = JSON.parse(body);
+  body = JSON.parse(body).results;
 
   for (let i = 0; i < body.length; ++i) {
     const chars = body[i].characters;
