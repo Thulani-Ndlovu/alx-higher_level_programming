f9220a33b6c5cb280dd6fba2344bfd39921b1245
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-11-01 20:09:59 +0200

    Task 0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-fs.readFile(process.arg[2], 'utf8', function (e, info_) {
+fs.readFile(process.argv[2], 'utf8', function (e, info_) {
   if (e) {
     console.log(e);
   } else {
