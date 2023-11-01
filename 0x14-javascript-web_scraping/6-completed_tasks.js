d9c9809bb7b0006ec74a32c217c1a7f54642ea01
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-11-01 21:43:58 +0200

    Task 6. How many completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], (e, res, body) => {
+  if (e) {
+    console.log(e);
+  } else {
+    const tasksCompletedByUserId = {};
+    body = JSON.parse(body);
+
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const complete = body[i].complete;
+
+      if (complete && !tasksCompletedByUserId[userId]) {
+        tasksCompletedByUserId[userId] = 0;
+      }
+      if (complete) ++tasksCompletedByUserId[userId];
+    }
+
+    console.log(tasksCompletedByUserId);
+  }
+});
